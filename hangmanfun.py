7a2f77f8706ae7a1828927315660bd7042b45ce0
saar <saarx1331@gmail.com>
2022-01-31 16:10:37 +0200

    first hangman game

--- hangmanfun.py
+++ hangmanfun.py
@@ -16,7 +16,10 @@ old_letters_guessed = []
 
 
 def check_valid_input(letter_guessed, old_letters_guessed):
-    """input check from user"""
+    """input check from user, it first check if the the string from the user is 1 if not it return false
+    second it check that there is no sign in the string if yes return False.
+    thirtd it check if the string is in the old_letter_guessed if yes return false.
+    forth it check if the string is in the alpabet and not in the old_letter_guessed list and return True"""
     if len(letter_guessed) != 1:
         return False
     elif letter_guessed not in 'abcdefghijklmnopqrstuvwxyz':
@@ -29,11 +32,15 @@ def check_valid_input(letter_guessed, old_letters_guessed):
 
 
 def not_vaild(old_letters_guessed):
+    """"print X and add to list "->" between letters"""
     print("X")
     print(" -> ".join(sorted(old_letters_guessed)))
 
 
 def try_update_letter_guessed(letter_guessed, old_letters_guessed):
+    """function that check if the letter work by the check_vaild_input function. 
+    and check if the letter show in the old_letter_guessed, if not then add it to the old letter list and return True.
+    if the letter show in the old_letter_guessed list then it print the not_vaild function with all the list and return False"""
     if check_valid_input(letter_guessed, old_letters_guessed) == True:
         old_letters_guessed.append(letter_guessed)
         print(old_letters_guessed)
